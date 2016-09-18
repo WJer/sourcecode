1fe8ad6a277ec2773936d9b378844ef68d6808f7
wujing <wujing@fxiaoke.com>
2016-09-18 21:12:46 +0800

    lazyload

---
+++ lazyload/js/lazyload.js
@@ -0,0 +1,14 @@
+(function(win,$){
+
+	$.fn.lazyload = function(opts){
+		var defaults = {
+			attr: 'real-url',
+			parent: win,
+			lazyX: 200, //懒加载横间距
+			lazyY: 200	//懒加载纵间距
+		};
+
+	}
+
+
+})(window,$);
